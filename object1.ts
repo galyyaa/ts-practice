711841a0d6822adc90be0ed1d32abf461b47d376
Galya Morgun <galya_morgun@MacBook-Air-Galya.local>
2024-04-30 10:52:17 +0300

    typescript-practice commit

---
+++ object1.ts
@@ -0,0 +1,2 @@
+const book = new Book('Harry Potter', 'J.K. Rowling', 1997);
+console.log(book.getSummary());
---
+++ object2.ts
@@ -0,0 +1,2 @@
+const electronicBook = new ElectronicBook('Harry Potter', 'J.K. Rowling', 1997, 'PDF');
+console.log(electronicBook.getSummary());
